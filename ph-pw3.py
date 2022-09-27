47394e9f505eba19237a48c4605fd9395b39873d
allexkap <aekpstn@gmail.com>
2022-09-28 00:34:26 +0300

    add jit (ph)

--- ph-pw3.py
+++ ph-pw3.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 import numpy as np
+from numba import jit
 
 
 v0 = 1000
@@ -8,8 +9,8 @@ q0 = 1.6 * 10**-19
 m0 = 1.66 * 10**-27
 ms = [i for i in range(20, 25)]
 
-dt = 10**-8
-N = 10**4
+dt = 10**-9
+N = 10**5
 
 n = len(ms)
 m = np.array([ms]).T * m0
@@ -20,9 +21,12 @@ B = np.zeros(3)
 v[:, 1] = v0
 B[2] = B0
 
-for i in range(1, N):
-    v += q0 * np.cross(v, B) / m * dt
-    x[i] = x[i-1] + v*dt
+@jit(nopython=True, cache=True)
+def run(N, m, x, v, B, q0, dt):
+    for i in range(1, N):
+        v += q0 * np.cross(v, B) / m * dt
+        x[i] = x[i-1] + v*dt
+run(N, m, x, v, B, q0, dt)
 
 plt.plot(x[:, :, 0], x[:, :, 1], '-')
 plt.show()
