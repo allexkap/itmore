62b3d80178727901f822f7e67adb04a38f0bdacd
allexkap <aekpstn@gmail.com>
2022-10-06 10:57:06 +0300

    undo jit (ph)

--- ph-pw3.py
+++ ph-pw3.py
@@ -1,6 +1,5 @@
 from matplotlib import pyplot as plt
 import numpy as np
-from numba import jit
 
 
 v0 = 1000
@@ -9,8 +8,8 @@ q0 = 1.6 * 10**-19
 m0 = 1.66 * 10**-27
 ms = [i for i in range(20, 25)]
 
-dt = 10**-9
-N = 10**5
+dt = 10**-6
+N = 10**2
 
 n = len(ms)
 m = np.array([ms]).T * m0
@@ -21,12 +20,9 @@ B = np.zeros(3)
 v[:, 1] = v0
 B[2] = B0
 
-@jit(nopython=True, cache=True)
-def run(N, m, x, v, B, q0, dt):
-    for i in range(1, N):
-        v += q0 * np.cross(v, B) / m * dt
-        x[i] = x[i-1] + v*dt
-run(N, m, x, v, B, q0, dt)
+for i in range(1, N):
+    x[i] = x[i-1] + (v := v + q0 * np.cross(v, B) / m * dt) * dt
 
-plt.plot(x[:, :, 0], x[:, :, 1], '-')
+plt.plot(x[:, :, 0], x[:, :, 1], '-', label=tuple(f'{m} Da' for m in ms))
+plt.legend()
 plt.show()
